00624b1ffe827daacd017fe365885bee83844297
anirudh-gowdar <anirudh.600@gmail.com>
2020-11-22 20:38:32 +0530

    Initial project setup

---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.http import request
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'home/index.html')
+
+
+def about(request):
+    return render(request, 'home/about.html')
+
