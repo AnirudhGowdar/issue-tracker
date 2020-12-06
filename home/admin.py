939d4f0311b05f8d92810eaf857405a25e99da77
anirudh-gowdar <anirudh.600@gmail.com>
2020-12-06 20:10:38 +0530

    Add models to admin view

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from . import models
 
-# Register your models here.
+admin.site.site_header = 'Issue Tracker'
+admin.site.site_title = 'Issue Tracker'
+admin.site.index_title = 'Admin Dashboard'
+admin.site.unregister(Group)
+admin.site.register(models.Project)
+admin.site.register(models.Ticket)
+
+
+admin.site.register(models.TicketType)
+admin.site.register(models.TicketPriority)
+admin.site.register(models.TicketStatus)
+admin.site.register(models.TicketHistory)
+admin.site.register(models.TicketComment)
