54880180e97e79e5345ebd5581fa24c94e7fbda5
anirudh-gowdar <anirudh.600@gmail.com>
2020-12-11 18:12:55 +0530

    Add project details page

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+
+app_name = 'projects'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('create', views.Create, name='Create'),
+    path('create', views.create, name='create'),
+    path('details/<int:project_id>', views.details, name='details')
 ]
--- projects/views.py
+++ projects/views.py
@@ -5,7 +5,7 @@ from . import forms
 from home.models import Project
 
 
-def Create(request):
+def create(request):
     form = None
     if request.method == 'POST':
         form = forms.CreateProjectForm(request.POST)
@@ -13,7 +13,7 @@ def Create(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Project Created Successfully')
-            return redirect('index')
+            return redirect('projects:index')
     elif request.method == 'GET':
         form = forms.CreateProjectForm()
     return render(
@@ -25,4 +25,12 @@ def Create(request):
 
 def index(request):
     tabledata = Project.objects.all()
+    for d in tabledata:
+        print(d.pk)
     return render(request, 'projects/index.html', {'tabledata': tabledata})
+
+
+def details(request, project_id):
+    print(project_id)
+    project = Project.objects.get(pk=project_id)
+    return render(request, 'projects/details.html', {'project': project})
