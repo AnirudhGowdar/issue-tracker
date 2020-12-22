c437815c23f58498de740a343f5068dcd52fa24a
anirudh-gowdar <anirudh.600@gmail.com>
2020-12-22 11:22:57 +0530

    Add migration to create groups

--- home/admin.py
+++ home/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
 from . import models
 
 admin.site.site_header = 'Issue Tracker'
 admin.site.site_title = 'Issue Tracker'
 admin.site.index_title = 'Admin Dashboard'
-admin.site.unregister(Group)
 admin.site.register(models.Project)
 admin.site.register(models.Ticket)
 
